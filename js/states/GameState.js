2872699221a074aa15851c7c3cc8c5df903efbec
Unknown <loic.dessaules@gmail.com>
2017-12-11 13:24:16 +0100

    Added a player and camera

--- js/states/GameState.js
+++ js/states/GameState.js
@@ -4,14 +4,48 @@ class GameState extends Phaser.State {
     }
 
     preload(){
-
+        game.load.image('background','img/backgrounds/debug-grid.png');
+        game.load.image('player','img/player.png');
     }
 
     create() {
+        this.deadZone = new Phaser.Rectangle(150, 150, 500, 300);
 
+        game.add.tileSprite(0, 0, 1920, 1920, 'background');
+        game.world.setBounds(0, 0, 1920, 1920);
+
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        this.player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+        game.physics.enable(this.player, Phaser.Physics.ARCADE);
+        this.player.body.collideWorldBounds = true;
+
+        this.cursors = game.input.keyboard.createCursorKeys();
+
+        game.camera.follow(this.player);
+        game.camera.deadzone = this.deadZone;
     }
 
     update(){
+        //  Reset the players velocity (movement)
+        this.player.body.velocity.x = 0;
+        this.player.body.velocity.y = 0;
 
+        //  Move to the left
+        if (this.cursors.left.isDown){
+            this.player.body.velocity.x = -250;
+        }
+        //  Move to the right
+        else if (this.cursors.right.isDown){
+            this.player.body.velocity.x = 250;
+        }
+        //  Move to the up
+        else if (this.cursors.down.isDown){
+            this.player.body.velocity.y = 250;
+        }
+        //  Move to the down
+        else if (this.cursors.up.isDown){
+            this.player.body.velocity.y = -250;
+        }
     }
 }
