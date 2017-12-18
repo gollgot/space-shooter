5aab73703647686465a66099ba4ed51f835fd753
Unknown <loic.dessaules@gmail.com>
2017-12-18 08:43:43 +0100

    Added assets of sprites and added player / background

--- js/states/GameState.js
+++ js/states/GameState.js
@@ -4,24 +4,30 @@ class GameState extends Phaser.State {
     }
 
     preload(){
-        game.load.image('background','img/backgrounds/debug-grid.png');
-        game.load.image('player','img/player.png');
+        game.load.image('background','img/backgrounds/bg-game.jpg');
+        game.load.image('player','img/game/player.png');
     }
 
     create() {
+        // Create the deadzone (zone in center that the player can move)
         this.deadZone = new Phaser.Rectangle(150, 150, 500, 300);
-
+        // Background (all the world)
         game.add.tileSprite(0, 0, 1920, 1920, 'background');
         game.world.setBounds(0, 0, 1920, 1920);
 
+        // Start the physics
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
+        // Create the player / set the anchor at the center of the sprite / Enable the physics on it / collide with world bounds
         this.player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+        this.player.anchor.setTo(0.5, 0.5);
         game.physics.enable(this.player, Phaser.Physics.ARCADE);
         this.player.body.collideWorldBounds = true;
 
+        // Create the cursors dor input keyboard
         this.cursors = game.input.keyboard.createCursorKeys();
 
+        // Camera settings
         game.camera.follow(this.player);
         game.camera.deadzone = this.deadZone;
     }
